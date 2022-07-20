9ab3f5b74a89430637bc800f249f988052a2bc94
EmilyFrancis1031 <60359470+EmilyFrancis1031@users.noreply.github.com>
2022-07-20 17:07:48 -0300

    Basic App layout Defined
    Basic layout of the app is defined. Containers for emoji, emoji editors, and emoji components are outlined.

--- twemoji-maker-app/App.js
+++ twemoji-maker-app/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.emojiViewContainer} />
+      <View style={styles.emojiEditorsContainer} />
+      <View style={styles.emojiComponentsContainer} />
+
+
     </View>
   );
 }
@@ -15,6 +19,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  emojiViewContainer:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height*0.35,
+  },
+  emojiEditorsContainer:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height*0.35,
+  },
+  emojiComponentsContainer:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height*0.35,
+  }
+
 });
